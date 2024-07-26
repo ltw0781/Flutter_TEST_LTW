91c2f5ea3a7c392045a4addc7ce3185d26497fd8
ltw0781 <ltw0781@naver.com>
2024-07-26 09:51:29 +0900

    2024-07-26

---
+++ sql.sql
@@ -0,0 +1,11 @@
+-- Active: 1717746144890@@127.0.0.1@3306@board
+CREATE TABLE `board` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `writer` varchar(100) NOT NULL,
+  `content` text,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `views` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`no`)
+);
